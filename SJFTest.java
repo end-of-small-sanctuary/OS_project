661cc84e6cb4bebc2ea9d060ab352ebe12c72eb1
end-of-small-sanctuary <166191066+end-of-small-sanctuary@users.noreply.github.com>
2024-04-25 17:28:59 +0300

    Update SJFTest.java

--- SJFTest.java
+++ SJFTest.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-
-//this draft runs SJF and RR correctly and consistently, Check wether non preemptive SJF rule works if Q1 is populated
+//both this and the previous draft skip over the second Queue when run, i've yet to try solving this issue
+//it will correctly run Q1 though
 
 /*
 Refrences used:
@@ -24,8 +24,9 @@ public class SJFTest {
     private static int processIndex=0;
     private static PCB[] Q1; 
     public static PCB[] Q2;
-    private static int totalProcessesQ1= 0;
-    private static int totalProcessesQ2= 0;
+    private static int Q1Processes= 0;
+    private static int Q2Processes= 0;
+    private static int totalProcesses= 0;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -42,15 +43,13 @@ public class SJFTest {
             
             switch (choice) {
                 case 1:
-                     System.out.print("Enter total number of processes for Q1: ");
-                    totalProcessesQ1 = scanner.nextInt();
-                    System.out.print("Enter total number of processes for Q2: ");
-                    totalProcessesQ2 = scanner.nextInt();
-                    enterProcessInfo(totalProcessesQ1, totalProcessesQ2, scanner);
+                     System.out.print("Enter total number of processes: ");
+                     totalProcesses = scanner.nextInt();
+                    enterProcessInfo(totalProcesses, scanner);
                     break;                   
                 case 2:
                     if ( Q1!=null || Q2 != null) {
-                        reportDetailedInfo(Q1,Q2,totalProcessesQ1,totalProcessesQ2);
+                        reportDetailedInfo(Q1,Q2,totalProcesses);
                     } else {
                         System.out.println("Please enter process information first.");
                     }
@@ -66,33 +65,12 @@ public class SJFTest {
     }
 
    //enter information loop method 
-    private static void enterProcessInfo(int totalProcessesQ1,int totalProcessesQ2, Scanner scanner) {
+    private static void enterProcessInfo(int totalProcesses, Scanner scanner) {
     
-        Q1 = new PCB[totalProcessesQ1];
-        Q2 = new PCB[totalProcessesQ2];
-       int totalProcesses=0;
-         for (int i = 0; i < totalProcessesQ1; i++) {
-                 if(totalProcessesQ1==0)
-                 break;
-                     System.out.println("\nFirst Priority " + (i + 1) + ":");
-                     
-                     System.out.print("Enter priority (1 or 2): ");
-                     int priority = scanner.nextInt();
-                     System.out.print("Enter arrival time: ");
-                     int arrivalTime = scanner.nextInt();
-                     System.out.print("Enter CPU burst time: ");
-                     int cpuBurst = scanner.nextInt();
-         
-                     //creates an object then inserts it into the array in each loop iteration
-                     Q1[i] = new PCB("P" + (++totalProcesses), priority, arrivalTime, cpuBurst);
-                    
-                                         }
-         
-                                        
-        for (int i = 0; i < totalProcessesQ2; i++) {
-        if(totalProcessesQ2==0)
-        break;
-            System.out.println("\nSecond Priority " + (i + 1) + ":");
+        Q1 = new PCB[totalProcesses];
+        Q2 = new PCB[totalProcesses];
+      for (int i = 0; i < totalProcesses; i++) {
+            System.out.println("\nProcess " + (i + 1) + ":");
             System.out.print("Enter priority (1 or 2): ");
             int priority = scanner.nextInt();
             System.out.print("Enter arrival time: ");
@@ -101,14 +79,19 @@ public class SJFTest {
             int cpuBurst = scanner.nextInt();
 
             //creates an object then inserts it into the array in each loop iteration
-            Q2[i] = new PCB("P" + (++totalProcesses), priority, arrivalTime, cpuBurst);
-            
+            PCB process = new PCB("P" + (i + 1), priority, arrivalTime, cpuBurst);
+            if (priority == 1){
+                Q1[i] = process;
+                Q1Processes++;}
+            else{
+                Q2[i] = process;
+                Q2Processes++;}
 }
     }
 
 
     //report detailed process method
-        private static void reportDetailedInfo(PCB[]Q1, PCB[] Q2,int totalProcessesQ1,int totalProcessesQ2) {
+        private static void reportDetailedInfo(PCB[]Q1, PCB[] Q2,int totalProcesses) {
         try {
         PrintWriter writer = new PrintWriter("Report1.txt");
 
@@ -116,6 +99,7 @@ public class SJFTest {
         StringBuilder schedulingOrder = new StringBuilder();
         StringBuilder processData = new StringBuilder();
         int currentTime = 0;
+        int timer=0;
         int totalTurnaroundTime = 0;
         int totalWaitingTime = 0;
         int totalResponseTime = 0;
@@ -129,12 +113,17 @@ do {
             done = true;      
       
 //Q1 algorithm
-if (Q1 != null) // add arrival time check, itll start process regardless of arrival time now
-for (int j = 0; j < Q1.length; j++) {
+if (Q1 != null)
+for (int j = 0; j < Q1Processes; j++) {
+timer++;
     if (Q1[j] == null)
         continue;
     if (Q1[j] .cpuBurst > 0 ) {
      done = false;
+     if(Q1[j].arrivalTime>timer){
+     timer++;
+     continue;
+     }
     //added here as to append the cpuBurst before any change, might move to the end of this method if i add a temp burst value
     processData.append(Q1[j].processID).append("\n").append(Q1[j].priority).append("\n").append(Q1[j].arrivalTime).append("\n").append(Q1[j].cpuBurst).append("\n"); 
        
@@ -155,7 +144,9 @@ for (int j = 0; j < Q1.length; j++) {
             totalWaitingTime += Q1[j].waitingTime;
             totalResponseTime += Q1[j].responseTime;
             Q1[j] = null;
+            
        }
+       timer+=currentTime;
        } 
     }
     
@@ -166,34 +157,34 @@ for (int j = 0; j < Q1.length; j++) {
 if (Q2 != null)
  do {
             done = true;
-            if(processIndex>=totalProcessesQ2)
-            break;
-       for (int l = 0; l <Q2.length; l++) {
-        
-       if(Q2[l] == null )
+
+       for (int l = 0; l <Q2Processes; l++) {
+       timer++;
+       if(Q2[l] == null || processIndex >= Q2Processes)
        break;
-       if(l !=0)
+       if(l !=0 || Q2[l].arrivalTime<timer)
        Arrays.sort(Q2);
          
         if (Q2[l].arrivalTime <= currentTime ) {
         processIndex++;
          done = false;
         //same with the previous algorithm
-        processData.append(Q2[l] .processID).append("\n").append(Q2[l] .priority).append("\n").append(Q2[l] .arrivalTime).append("\n").append(Q2[l] .cpuBurst);
+        processData.append("\n").append(Q2[l] .processID).append("\n").append(Q2[l] .priority).append("\n").append(Q2[l] .arrivalTime).append("\n").append(Q2[l] .cpuBurst);
            
             if (Q2[l] .responseTime == 0)
                 Q2[l].responseTime = currentTime - Q2[l] .arrivalTime;
             Q2[l] .startTime = currentTime;
             schedulingOrder.append(Q2[l] .processID).append(" | ");
             currentTime += Q2[l] .cpuBurst;
-            Q2[l] .terminationTime = currentTime;
-            Q2[l] .turnaroundTime = Q2[l] .terminationTime - Q2[l] .arrivalTime;
+            Q2[l].terminationTime = currentTime;
+            Q2[l].turnaroundTime = Q2[l] .terminationTime - Q2[l] .arrivalTime;
             totalTurnaroundTime += Q2[l] .turnaroundTime;
-            Q2[l] .waitingTime = Q2[l] .turnaroundTime - Q2[l] .cpuBurst;
-            totalWaitingTime += Q2[l] .waitingTime;
+            Q2[l].waitingTime = Q2[l] .turnaroundTime - Q2[l] .cpuBurst;
+            totalWaitingTime += Q2[l].waitingTime;
             totalResponseTime += Q2[l] .responseTime;
             
         }
+        timer+=currentTime;
     }
     
  } while (!done);
